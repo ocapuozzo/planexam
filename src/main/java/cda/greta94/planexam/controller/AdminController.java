42b5ab80ab0e182c136e1c2c326728b3a282ec43
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2023-06-20 12:29:07 +0200

    add premier pas de l'import, vue en cours aujourd'hui

--- src/main/java/cda/greta94/planexam/controller/AdminController.java
+++ src/main/java/cda/greta94/planexam/controller/AdminController.java
@@ -3,12 +3,18 @@ package cda.greta94.planexam.controller;
 import cda.greta94.planexam.dto.EtablissementDto;
 import cda.greta94.planexam.service.EtablissementService;
 import jakarta.validation.Valid;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.*;
 
 @RequestMapping("/admin")
 @Controller
@@ -34,19 +40,10 @@ public class AdminController {
         return "etablissement/form";
     }
 
-
-  @GetMapping(value = "/etablissement/edit/{id}")
-  public String pushFormEtab(@PathVariable("id") long id, Model model){
-    EtablissementDto etablissementDto = etablissementService.findEtablissementDtoById(id);
-    model.addAttribute("etablissementDto", etablissementDto);
-    return "etablissement/form";
-  }
-
-
   @PostMapping(value = "/etablissement")
     public String addUpdateEtab(@Valid @ModelAttribute EtablissementDto etablissementDto, BindingResult bindingResult) {
-       logger.info("addUpdateEtab (" + etablissementDto + ")");
-       logger.info("hasErrors = " + bindingResult.hasErrors());
+       logger.info("addUpdateEtab : (" + etablissementDto + ")");
+       logger.info("hasErrors = "  + bindingResult.hasErrors());
 
        if(bindingResult.hasErrors()) {
          return "etablissement/form";
@@ -55,4 +52,53 @@ public class AdminController {
        etablissementService.saveEtablissementFromEtablissementDto(etablissementDto);
        return "redirect:/admin/etablissements";
     }
+
+  @GetMapping(value = "/etablissement/edit/{id}")
+  public String pushFormEtab(@PathVariable("id") long id, Model model){
+    EtablissementDto etablissementDto = etablissementService.findEtablissementDtoById(id);
+    model.addAttribute("etablissementDto", etablissementDto);
+    return "etablissement/form";
+  }
+
+  @GetMapping(value = "/etablissement/import")
+  public String formImportCSV(){
+    return "/etablissement/formImportCSV";
+  }
+
+  @PostMapping(value = "/etablissement/import")
+  public String importCSV(@RequestParam("file") MultipartFile file,  RedirectAttributes redirAttrs){
+
+    if (file.isEmpty()) {
+      // PATTERN PRG
+      redirAttrs.addFlashAttribute("errorMessage", "Please select a file to upload");
+      return "redirect:/admin/etablissement/import";
+    }
+    // traitement du fichier CSV
+    Reader in = null;
+    try {
+//      in = new FileReader();
+      Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(new InputStreamReader(file.getInputStream()));
+      for (CSVRecord record : records) {
+        // pour chaque enregistrement
+        // Création d'un objet EtablissementDto // pour la ville voir en second
+        // vérifier que l'obbet n'est pas dans la base de données (utiliser une des clés candidates)
+        // si non présent faire un save
+
+        // TODO logique de l'impotr à implémenter dans une classe service
+        
+        String columnOne = record.get(0);
+        String columnTwo = record.get(1);
+        logger.info("C0 :" + columnOne);
+        logger.info("C1 :" + columnTwo);
+      }
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    redirAttrs.addFlashAttribute("errorMessage", "Importation à implémenter !");
+    return "redirect:/admin/etablissements";
+  }
+
 }
